cf017b237939279455283bb265484b5799242824
SamSkull19 <sifatsamin19@gmail.com>
2024-02-19 22:32:52 +0600

    Main Page Functionality Done

--- js/custom.js
+++ js/custom.js
@@ -1,61 +1,145 @@
-// Seat  Color Added
+const btnSelect = document.getElementsByClassName('btnSelect');
 
-const a1Select = document.getElementById('a1Select');
+let count = 0;
+let totalSeatAvailable = 8;
 
-a1Select.addEventListener('click',function(){
-    a1Select.style.backgroundColor = '#1DD100';
-    a1Select.style.color = 'white';
-});
+for (const btn of btnSelect) {
+    btn.addEventListener('click', function (event) {
+        count++;
+        totalSeatAvailable--;
 
-const a2Select = document.getElementById('a2Select');
-
-a2Select.addEventListener('click',function(){
-    a2Select.style.backgroundColor = '#1DD100';
-    a2Select.style.color = 'white';
-});
-
-const b1Select = document.getElementById('b1Select');
-
-b1Select.addEventListener('click',function(){
-    b1Select.style.backgroundColor = '#1DD100';
-    b1Select.style.color = 'white';
-});
-
-const b2Select = document.getElementById('b2Select');
-
-b2Select.addEventListener('click',function(){
-    b2Select.style.backgroundColor = '#1DD100';
-    b2Select.style.color = 'white';
-});
+        let seatPrice = 550;
 
 
-const a3Select = document.getElementById('a3Select');
+        if (count <= 4) {
+            btn.style.backgroundColor = '#1DD100';
+            btn.style.color = 'white';
 
-a3Select.addEventListener('click',function(){
-    a3Select.style.backgroundColor = '#1DD100';
-    a3Select.style.color = 'white';
-});
+            const totalSeatCart = count;
+            setInnerText('seatOnCart', totalSeatCart);
 
-const a4Select = document.getElementById('a4Select');
+            const seatReducing = totalSeatAvailable;
+            setInnerText('totalSeatAvailable', seatReducing);
 
-a4Select.addEventListener('click',function(){
-    a4Select.style.backgroundColor = '#1DD100';
-    a4Select.style.color = 'white';
-});
+            // Cart List
+            const cartL = document.getElementById('cartList');
 
-const b3Select = document.getElementById('b3Select');
+            const li = document.createElement('li');
 
-b3Select.addEventListener('click',function(){
-    b3Select.style.backgroundColor = '#1DD100';
-    b3Select.style.color = 'white';
-});
+            const p1 = document.createElement('p');
+            p1.innerText = btn.innerText;
 
-const b4Select = document.getElementById('b4Select');
+            const p2 = document.createElement('p');
+            p2.innerText = 'Economy';
 
-b4Select.addEventListener('click',function(){
-    b4Select.style.backgroundColor = '#1DD100';
-    b4Select.style.color = 'white';
-});
+            const p3 = document.createElement('p');
+            p3.innerText = '550';
+
+            li.appendChild(p1);
+            li.appendChild(p2);
+            li.appendChild(p3);
+
+            cartL.appendChild(li);
+
+            li.style.display = 'flex';
+            li.style.justifyContent = 'space-between';
+            li.style.alignItems = 'center';
+
+            totalPriceCost('totalPrice', parseInt(seatPrice));
+            totalGrandPriceCostCoupon('others');
+
+        
+
+
+        }
+        else {
+            // const warn = document.getElementById('seatMain'); 
+            // const p = document.createElement('p');
+            // p.innerText = 'Maximum 4 seats can be selected.'
+            // warn.appendChild(p);
+
+            btn.setAttribute('disabled', true);
+        }
+
+    });
+}
+
+const delCon = document.getElementById('inputCoupon');
+
+delCon.addEventListener('keyup', function (event) {
+    const inputValue = event.target.value;
+
+    const delBTN = document.getElementById('enterD')
+    if (inputValue === 'NEW15' || inputValue === 'Couple20') {
+        delBTN.removeAttribute('disabled');
+        totalGrandPriceCostCoupon(inputValue);
+    }
+    else {
+        delBTN.setAttribute('disabled', true);
+    }
+
+})
+
+
+const enter = document.getElementById('enterD');
+
+enter.addEventListener('click', function () {
+    const inputDel = document.getElementById('inputDel');
+    const texts = document.getElementById('textT');
+})
+
+const enterFinal = document.getElementById('enterD');
+enterFinal.addEventListener('click', function () {
+    const couponBox = document.getElementById('CouponBox');
+    couponBox.style.visibility= 'hidden';
+})
+
+console.log(totalPrice);
+
+function totalPriceCost(id, price) {
+    let totalCost = document.getElementById(id).innerText;
+    let convertedCost = parseInt(totalCost);
+    const sum = convertedCost + parseInt(price);
+    setInnerText('totalPrice', sum);
+}
+
+
+
+
+function totalGrandPriceCostCoupon(category) {
+    let totalCost = document.getElementById('totalPrice').innerText;
+    let convertedCost = parseInt(totalCost);
+
+    let dis = 0;
+    let sum = convertedCost;
+    if (category === 'NEW15') {
+        dis = ((convertedCost * 15) / 100);
+        sum = sum - dis;
+        setInnerText('grandPrice', sum);
+    }
+
+    else if (category === 'Couple20') {
+        dis = ((convertedCost * 20) / 100);
+        sum = sum - dis;
+        setInnerText('grandPrice', sum);
+    }
+
+    else {
+        dis = convertedCost;
+        setInnerText('grandPrice', dis);
+    }
+}
+
+
+
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+}
+
+
+
+
+
 
 
 
