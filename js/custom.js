b9ac4124851f0d59a98711612de35dfe33c75b7e
SamSkull19 <sifatsamin19@gmail.com>
2024-02-19 17:34:42 +0600

    Seat selection color added

---
+++ js/custom.js
@@ -0,0 +1,68 @@
+// Seat  Color Added
+
+const a1Select = document.getElementById('a1Select');
+
+a1Select.addEventListener('click',function(){
+    a1Select.style.backgroundColor = '#1DD100';
+    a1Select.style.color = 'white';
+});
+
+const a2Select = document.getElementById('a2Select');
+
+a2Select.addEventListener('click',function(){
+    a2Select.style.backgroundColor = '#1DD100';
+    a2Select.style.color = 'white';
+});
+
+const b1Select = document.getElementById('b1Select');
+
+b1Select.addEventListener('click',function(){
+    b1Select.style.backgroundColor = '#1DD100';
+    b1Select.style.color = 'white';
+});
+
+const b2Select = document.getElementById('b2Select');
+
+b2Select.addEventListener('click',function(){
+    b2Select.style.backgroundColor = '#1DD100';
+    b2Select.style.color = 'white';
+});
+
+
+const a3Select = document.getElementById('a3Select');
+
+a3Select.addEventListener('click',function(){
+    a3Select.style.backgroundColor = '#1DD100';
+    a3Select.style.color = 'white';
+});
+
+const a4Select = document.getElementById('a4Select');
+
+a4Select.addEventListener('click',function(){
+    a4Select.style.backgroundColor = '#1DD100';
+    a4Select.style.color = 'white';
+});
+
+const b3Select = document.getElementById('b3Select');
+
+b3Select.addEventListener('click',function(){
+    b3Select.style.backgroundColor = '#1DD100';
+    b3Select.style.color = 'white';
+});
+
+const b4Select = document.getElementById('b4Select');
+
+b4Select.addEventListener('click',function(){
+    b4Select.style.backgroundColor = '#1DD100';
+    b4Select.style.color = 'white';
+});
+
+
+
+
+
+
+
+
+
+
